9b83807d1532556e8f6277b02e5679babd1cf2d6
EliasLegui <leguizamon.elias.95@gmail.com>
2022-08-21 10:29:37 -0300

    1

--- proyecto-final/src/app/app.module.ts
+++ proyecto-final/src/app/app.module.ts
@@ -8,6 +8,12 @@ import { LogoComponent } from './components/logo/logo.component';
 import { SocialsComponent } from './components/socials/socials.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ProyectoComponent } from './components/proyecto/proyecto.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -16,11 +22,17 @@ import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
     LogoComponent,
     SocialsComponent,
     BannerComponent,
-    AcercaDeComponent
+    AcercaDeComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    SkillsComponent,
+    ProyectoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
